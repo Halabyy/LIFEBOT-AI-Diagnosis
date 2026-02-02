3853522360a1ff37f6ab2b15d6ed492057e227b9
Halabyy <110264640+Halabyy@users.noreply.github.com>
2026-02-02 21:04:44 +0200

    Add files via upload

---
+++ src/test/java/com/trustr/middleware/core_middleware/CoreMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustr.middleware.core_middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
